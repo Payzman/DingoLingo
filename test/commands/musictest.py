5832a2495e051a3440b6dc449863235a83aa6186
Tim Pasemann <tim.pasemann@gmx.net>
2022-07-03 22:37:52 +0200

    Add message + context (WIP)

--- test/commands/musictest.py
+++ test/commands/musictest.py
@@ -1,10 +1,24 @@
-import musicbot.commands.music as music
+import discord.ext
+import musicbot.commands.music
 
 from unittest import IsolatedAsyncioTestCase
 
 
 class Test(IsolatedAsyncioTestCase):
     async def test_unknown_youtube_file(self):
-        music_instance = music.Music(None)
-        await music_instance._play_song(music_instance, None, track="https://open.spotify.com/album/1VNWqVr6mUMg177IODYb0T?si"
-                                                          "=XkXdpLrrTKmLitC0gS868g")
+        music = musicbot.commands.music.Music(None)
+        data = {
+            'id': '993251638783520838',
+            'attachments': [],
+            'embeds': [],
+            'edited_timestamp': None,
+            'type': discord.MessageType.default,
+            'pinned': False,
+            'mention_everyone': False,
+            'tts': False,
+            'content': ''
+        }
+        msg = discord.Message(state=None, channel=None, data=data)
+        ctx = discord.ext.commands.context.Context(message=msg, prefix=None)
+        await music._play_song(music, ctx, track="https://open.spotify.com/album"
+                                                 "/1VNWqVr6mUMg177IODYb0T?si=XkXdpLrrTKmLitC0gS868g")
