91dd3efb6de3d6e41314100ce5221d7392e3a367
Tim Pasemann <tim.pasemann@gmx.net>
2022-07-03 22:10:55 +0200

    Run async test

--- test/commands/musictest.py
+++ test/commands/musictest.py
@@ -1,7 +1,10 @@
 import musicbot.commands.music as music
 
+from unittest import IsolatedAsyncioTestCase
 
-def test_unknown_youtube_file():
-    music_instance = music.Music(None)
-    music_instance._play_song(None, None,
-                              "https://open.spotify.com/album/1VNWqVr6mUMg177IODYb0T?si=XkXdpLrrTKmLitC0gS868g")
+
+class Test(IsolatedAsyncioTestCase):
+    async def test_unknown_youtube_file(self):
+        music_instance = music.Music(None)
+        await music_instance._play_song(music_instance, None, track="https://open.spotify.com/album/1VNWqVr6mUMg177IODYb0T?si"
+                                                          "=XkXdpLrrTKmLitC0gS868g")
